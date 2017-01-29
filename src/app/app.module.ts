401555a8fab5f18ee93e2d49804ac3133ed8ea69
adeelibr <adeelimranr@gmail.com>
2017-01-29 20:42:26 +0500

    Shopping module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeListItemComponent } from './recipes/recipe-list/recipe-list-item/recipe-list-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListAddComponent } from './shopping-list/shopping-list-add/shopping-list-add.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,8 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
     RecipeListComponent,
     RecipeListItemComponent,
     RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListAddComponent,
   ],
   imports: [
     BrowserModule,
